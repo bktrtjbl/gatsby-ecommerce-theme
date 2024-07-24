0959daa552cb561b751a6ecdadac7cf3560ed6c9
bktrtjbl <balakrishnatrt.jbl@gmail.com>
2024-07-24 15:20:49 +0100

    small updates to test
    small updates to test

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,4 +4,4 @@ import { NotificationProvider } from './src/context/AddItemNotificationProvider'
 
 export const wrapRootElement = ({ element }) => (
   <NotificationProvider>{element}</NotificationProvider>
-);
+); 
